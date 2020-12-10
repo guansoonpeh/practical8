0e2478457697584398e6a3ec6b11439abf0a92c5
TARUC <TARUC@PC-82>
2020-12-10 13:56:09 +0800

    init

--- src/Q2/InvoiceLine.java
+++ src/Q2/InvoiceLine.java
@@ -1,6 +1,35 @@
 package Q2;
 
 public class InvoiceLine {
- 
 
+    private Product product;
+    private int quantity;
+    private double price;
+
+    public InvoiceLine() {
+
+    }
+
+    public InvoiceLine(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = quantity * product.getPrice();
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    
+    
+//    public String toString(){
+//
+//    }
 }
